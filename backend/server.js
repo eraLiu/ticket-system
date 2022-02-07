51882804fca350da392a930ccbfebf98786b83b7
eraLiu <76599055+eraLiu@users.noreply.github.com>
2022-02-07 22:07:37 +1300

    add routes & controller

--- backend/server.js
+++ backend/server.js
@@ -7,4 +7,6 @@ app.get('/', (req,res) =>{
     res.status(200).json({message:'welcome to the support api'})
 })
 
+app.use('/api/users', require('./routes/userRoutes'))
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
