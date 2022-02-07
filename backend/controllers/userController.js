bbd0bf4fb281368d2de3a7e7fc1b0e879ccce290
eraLiu <76599055+eraLiu@users.noreply.github.com>
2022-02-08 09:27:36 +1300

    protect routes & authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,13 +63,27 @@ const loginUser = asyncHandler(async(req, res) => {
         throw new Error('Invalid credentials')
     }
 })
+// @desc    Get current user
+// @route   /api/users/me
+// @access  Private
+const getMe = asyncHandler(async(req,res) =>{
+    const user = {
+        id: req.user._id,
+        email:req.user.email,
+        name:req.user.name,
+    }
+    res.status(200).json(user)
+})
 //Generate token
 const generateToken = (id) =>{
     return jwt.sign({id}, process.env.JWT_SECRET,{
         expiresIn:'30d'
     } )
 }
+
+
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    getMe
 }
