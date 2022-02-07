fb8c933bc849573d9e763bbef01c9fed9fb3f378
eraLiu <76599055+eraLiu@users.noreply.github.com>
2022-02-08 07:37:54 +1300

    error&exception handling

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const {errorHandler} = require('./middleware/errorMiddleware')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 8000
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 app.get('/', (req,res) =>{
     res.status(200).json({message:'welcome to the support api'})
 })
 
 app.use('/api/users', require('./routes/userRoutes'))
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
