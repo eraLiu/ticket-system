78615c4184bf36bdfe8543b56a1a4dc5860ed3ec
eraLiu <76599055+eraLiu@users.noreply.github.com>
2022-02-08 07:52:42 +1300

    connect database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const dotenv = require('dotenv').config()
+const colors = require('colors')
 const PORT = process.env.PORT || 8000
+
+connectDB()
 const app = express()
 
 app.use(express.json())
